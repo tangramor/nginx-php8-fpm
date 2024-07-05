95fa40a5deee9859959a939bfbf5e673212f2b54
tangramor <tangramor@gmail.com>
2024-07-05 11:03:53 +0800

    update to php8.3.8_node22.4.0

--- build.withoutNodejs.sh
+++ build.withoutNodejs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 docker build \
+    --build-arg APKMIRROR="mirrors.tuna.tsinghua.edu.cn" \
     -f Dockerfile.withoutNodejs \
     -t tangramor/nginx-php8-fpm:php8.3.8_withoutNodejs .
